c1f94ab434752015d37018a24dd92753f44b070b
99003326 <abhishek.guria@ltts.com>
2021-02-13 17:00:33 +0530

    some part of code done

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 void calculator_menu(void)
 {
     printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
+    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. GCD\n6. SINE\n7. COS\n8. TAN\n9. power\n10. log\n11. antilog\n12. Exit");
     printf("\n\tEnter your choice\n");
    
     scanf("%d", &calculator_operation);
@@ -89,7 +89,60 @@ void calculator_menu(void)
 
             getchar();
             break;
-        case 5:
+        case GCD:
+            printf("\n\t%d / %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            GCD(calculator_operand1, calculator_operand2));
+
+            getchar();
+            break;
+        case SINE:
+            printf("\n\t%d / %d = %d\nEnter to continue", 
+            calculator_operand1,
+            SIN(calculator_operand1));
+
+            getchar();
+            break;
+        case COS:
+            printf("\n\t%d / %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            COS(calculator_operand1));
+
+            getchar();
+            break;
+        case TAN:
+            printf("\n\t%d / %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            TAN(calculator_operand1));
+
+            getchar();
+            break;
+        case power:
+            printf("\n\t%d / %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            divide(calculator_operand1, calculator_operand2));
+
+            getchar();
+            break;
+        case log:
+            printf("\n\t%d / %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            
+            divide(calculator_operand1, calculator_operand2));
+
+            getchar();
+            break;    
+        case antilog:
+            printf("\n\t%d / %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            divide(calculator_operand1, calculator_operand2));
+
+            getchar();
+            break;    
+        case 12:
             exit(0);
             break;
         default:
--- 3_Implementation/src/calculator_operations.c
+++ 3_Implementation/src/calculator_operations.c
@@ -1,5 +1,5 @@
-#include <calculator_operations.h>
-
+#include<calculator_operations.h>
+#include<math.h>
 int add(int operand1, int operand2)
 {
     return operand1 + operand2;
@@ -21,4 +21,75 @@ int divide(int operand1, int operand2)
         return 0;
     else
         return operand1 / operand2;
+}
+int GCD(int operand1, int operand2)
+{
+   int i, GCD;
+
+  
+    for(i = 1; i <= operand1 && i <= operand2; i++)
+    {
+        if(operand1 % i == 0 && operand2 % i == 0)
+            GCD = i;
+    }
+
+     
+    return  GCD;
+}
+
+float LOG(float operand1)
+{
+ 
+        return log(operand1);
+}
+
+float ANTILOG(float operand1)
+{
+
+}
+
+float SINE(float operand1)
+{
+ float ret, val;
+
+
+   val = 3.141592/ 180;
+   ret = sin(operand1*val);
+  
+   
+   return(ret);
+       
+}
+
+
+float COS(float operand1)
+{
+ float ret, val;
+
+
+   val = 3.141592/ 180;
+   ret = cos(operand1*val);
+  
+   
+   return(ret);
+       
+}
+
+
+float TAN(float operand1)
+{
+ float ret, val;
+
+
+   val = 3.141592/ 180;
+   ret = tan(operand1*val);
+  
+   
+   return(ret);
+       
+}
+
+float power(float operand1, float operand2)
+{
+    return(pow(float operand1, float operand2));
 }
