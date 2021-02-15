1e8ea994172aac8af3dc64d6de9dbbedb33f3a78
99003723 <78869361+99003723@users.noreply.github.com>
2021-02-15 11:05:24 +0530

    lengthconv added further debug needed

--- 3_Implementation/src/calculator_operations.c
+++ 3_Implementation/src/calculator_operations.c
@@ -100,3 +100,21 @@ float cubeRoot(float operand1)
     for(--user_input;(user_input*user_input*user_input)<n;user_input=user_input+precision);
     return user_input;
 }
+
+float LengthConv(float operand1)
+{
+    float cm, meter, km;
+
+    /* Input length in centimeter from user */
+    printf("Enter length in centimeter: ");
+    scanf("%f", &cm);
+
+    /* Convert centimeter into meter and kilometer */
+    meter = cm / 100.0;
+    km    = cm / 100000.0;
+
+    printf("Length in Meter = %.2f m \n", meter);
+    printf("Length in Kilometer = %.2f km", km);
+}
+
+
